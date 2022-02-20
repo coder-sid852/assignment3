9c023ca7ac6998e9277f8eb1eb6e5241e1a0c0f2
coder-sid852 <96578178+coder-sid852@users.noreply.github.com>
2022-02-20 19:56:58 +0530

    Add files via upload

---
+++ feb14/first.java
@@ -0,0 +1,16 @@
+package feb14;
+
+public class first {
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count=0;
+        int i=0,j=0;
+        while(i<startTime.length && j<endTime.length){
+            if(startTime[i]<=queryTime && endTime[j]>=queryTime)
+                count++;
+            i++;
+            j++;
+        }
+        return count;
+    }
+
+}
---
+++ feb14/second.java
@@ -0,0 +1,12 @@
+package feb14;
+
+public class second {
+    public int[] runningSum(int[] nums) {
+        int[] ans = new int[nums.length];
+        ans[0] = nums[0];
+        for (int i = 1; i < nums.length; i++)
+            ans[i] = ans[i-1] + nums[i];
+        return ans;
+        }
+    
+}
